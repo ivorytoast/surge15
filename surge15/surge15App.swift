c157aa199d87c3b08582521dccbeb965e25063bb
Anthony Hamill <anthonyhamill3853@gmail.com>
2026-07-10 19:38:14 -0400

    Removed debug code

--- surge15/surge15App.swift
+++ surge15/surge15App.swift
@@ -32,13 +32,6 @@ struct surge15App: App {
         }
     }()
 
-    #if DEBUG
-    init() {
-        UserDefaults.standard.set(0, forKey: "onboardingPhase")
-        UserDefaults.standard.set(false, forKey: "hasSeenOnboarding")
-    }
-    #endif
-
     var body: some Scene {
         WindowGroup {
             RootView()
